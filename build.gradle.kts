f994ca2894356f0799b98ffd01aa344bb2e440b8
Artem Chepurnoy <artemchep@gmail.com>
2019-06-01 20:11:09 +0300

    Update gradle tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.5.0-beta01")
+        classpath("com.android.tools.build:gradle:3.5.0-beta03")
         classpath("io.realm:realm-gradle-plugin:5.8.0")
         classpath("com.google.gms:google-services:4.2.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$ANDROIDARCH_NAVIGATION_VERSION")

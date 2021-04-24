013b0f6f7e8f2a22bc7da40db976c53030fff1dd
Artem Chepurnoy <artemchep@gmail.com>
2021-04-24 11:10:06 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha08")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
         classpath("io.realm:realm-gradle-plugin:10.3.1")
         classpath("com.google.gms:google-services:4.3.5")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$ANDROIDARCH_NAVIGATION_VERSION")
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
-const val KOTLIN_VERSION = "1.4.30"
+const val KOTLIN_VERSION = "1.5.0-RC"
 
 // https://github.com/Kotlin/kotlinx.coroutines
-const val KOTLIN_COROUTINES_VERSION = "1.4.2"
+const val KOTLIN_COROUTINES_VERSION = "1.4.3"
 
 // https://github.com/nhaarman/mockito-kotlin
 const val KOTLIN_MOCKITO_VERSION = "2.2.0"
@@ -11,13 +11,13 @@ const val KLUENT_VERSION = "1.63"
 const val JUNIT_VERSION = "4.13.1"
 
 // https://kodein.org/
-const val KODEIN_VERSION = "7.3.0"
+const val KODEIN_VERSION = "7.5.0"
 
 // https://github.com/AChep/config
 const val ARTEMCHEP_CONFIG_VERSION = "2.2.0"
 
 // https://github.com/serso/android-checkout
-const val SOLOVYEV_CHECKOUT_VERSION = "1.2.2"
+const val SOLOVYEV_CHECKOUT_VERSION = "1.2.3"
 
 // https://github.com/hdodenhof/CircleImageView
 const val HDODENHOF_CIRCLEIMAGEVIEW_VERSION = "3.1.0"
@@ -32,26 +32,26 @@ const val GRENDERG_TOASTY_VERSION = "1.5.0"
 const val YAROLEGOVICH_DISCRETESCROLLVIEW_VERSION = "1.5.1"
 
 // https://github.com/mikepenz/FastAdapter
-const val MIKEPENZ_FASTADAPTER_VERSION = "5.3.4"
+const val MIKEPENZ_FASTADAPTER_VERSION = "5.4.1"
 
 // https://developer.android.com/kotlin/ktx
 const val ANDROIDX_KTX_VERSION = "1.3.2"
 
 // https://developer.android.com/jetpack/androidx/versions
-const val ANDROIDX_APPCOMPAT_VERSION = "1.3.0-beta01"
+const val ANDROIDX_APPCOMPAT_VERSION = "1.3.0-rc01"
 const val ANDROIDX_BROWSER_VERSION = "1.3.0"
 const val ANDROIDX_LIFECYCLE_VERSION = "2.2.0"
-const val ANDROIDX_CONSTRAINTLAYOUT_VERSION = "2.1.0-alpha2"
+const val ANDROIDX_CONSTRAINTLAYOUT_VERSION = "2.1.0-beta01"
 const val ANDROIDARCH_NAVIGATION_VERSION = "2.3.3"
 const val ANDROIDARCH_WORK_VERSION = "2.5.0"
 
 // https://mvnrepository.com/artifact/com.google.android.material/material
-const val GOOGLE_MATERIAL_VERSION = "1.3.0"
+const val GOOGLE_MATERIAL_VERSION = "1.4.0-alpha02"
 
 // https://github.com/ACRA/acra
 const val ACRA_VERSION = "5.7.0"
 
 // https://firebase.google.com/support/release-notes/android
-const val GOOGLE_FIREBASE_ANALYTICS = "18.0.2"
-const val GOOGLE_FIREBASE_FIRESTORE = "22.0.2"
+const val GOOGLE_FIREBASE_ANALYTICS = "18.0.3"
+const val GOOGLE_FIREBASE_FIRESTORE = "22.1.2"
 
